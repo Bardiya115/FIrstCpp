0108df34bb10fa2a6b128e46a8d5503ad3de5f58
Bardiya115 <bardiya_rabiei@yahoo.com>
2021-11-25 14:01:44 -0800

    Create main 2.cpp

---
+++ main 2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ double a; 
+ int b;
+ int c;
+ int d;
+ int e;
+ double f;
+  cout<<"variable 1:";
+  cin>> a;
+  cout<< "variable 2:";
+  cin>> b;
+   
+  c = a + b;
+cout<< "sum: "<<c<<endl;
+  d = a - b;
+  cout<< "difference: "<<d<<endl;
+  e = a * b;
+  cout<< "product: "<<e<<endl;
+  f = a / b;
+  cout<< "divisoin: "<<f<<endl;
+
+}
